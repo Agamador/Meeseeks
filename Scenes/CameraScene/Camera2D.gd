9089a4d9d149dc8640f30fdfd7cb6265e35ba582
Alejandro García <ale.garciamador@alum.uca.es>
2021-12-24 18:59:29 +0100

    Escena Cámara funcionando

---
+++ Scenes/CameraScene/Camera2D.gd
@@ -0,0 +1,67 @@
+extends Camera2D
+
+var viewport_size;
+var width_half
+var height_half
+var zoom_dif_h
+var zoom_dif_v
+var w_h_times_zoom
+var h_h_times_zoom
+var TileMapBorder
+
+export var tween_duration := 0.5
+export var max_zoom := 2.0
+export var min_zoom := 0.5
+var zoom_level := 1.0 setget _set_zoom_level
+var _zoom_factor := 0.1
+
+onready var tween : Tween = $Tween
+
+func _ready():
+	TileMapBorder = $"../TileMap".get_used_rect();
+	print(TileMapBorder.position);
+	self.limit_top = TileMapBorder.position.x *64;
+	self.limit_left = TileMapBorder.position.y * 64;
+	self.limit_right = TileMapBorder.end.x * 64;
+	self.limit_bottom = TileMapBorder.end.y * 64;
+	print(TileMapBorder.position.x);
+	viewport_size = get_viewport().size;
+	width_half = viewport_size.x/2;
+	height_half = viewport_size.y/2;
+	w_h_times_zoom = width_half * self.zoom.x;
+	h_h_times_zoom = height_half * self.zoom.y;
+
+func _process(delta):
+	var move_vector = Vector2();
+	var mouse_pos = get_viewport().get_mouse_position();
+
+	if mouse_pos.x < 30 && self.position.x - w_h_times_zoom > self.limit_left:
+		move_vector.x = -1;
+	elif mouse_pos.x > viewport_size.x - 30 && self.position.x + w_h_times_zoom < self.limit_right:
+		move_vector.x = 1;
+	if mouse_pos.y < 30 && self.position.y - h_h_times_zoom > self.limit_top:
+		move_vector.y = -1;
+	if mouse_pos.y > viewport_size.y - 30  && self.position.y + h_h_times_zoom < self.limit_bottom:
+		move_vector.y = 1;
+
+	global_translate(move_vector * delta * 300 * self.zoom.x)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("zoom_in"):
+		_set_zoom_level(zoom_level - _zoom_factor)
+	if event.is_action_pressed("zoom_out"):
+		_set_zoom_level(zoom_level + _zoom_factor)
+		
+#el zoom ahora mismo permite ver fuera del mapa.
+func _set_zoom_level(value: float): 
+	zoom_level = clamp(value, min_zoom, max_zoom);
+	tween.interpolate_property(
+		self, 
+		'zoom', 
+		zoom,
+		zoom_level * Vector2.ONE,
+		tween_duration,
+		Tween.TRANS_SINE,
+		Tween.EASE_OUT);
+	tween.start();
+	
---
+++ Scenes/Meesek.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+
+const DEADSPEED = 200
+const MAXSPEED = 60
+const GRAVITY = 150
+var right = 1
+var alive = true
+var air_time = 0
+var once = true
+#Se inicia el movimiento del misik en caida
+var motion = Vector2(0,GRAVITY)
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	#print($RayCast2D)
+	if(alive):
+		#si vuela durante más de 2 segundos aprox(5 bloques)
+		if air_time > 120: 
+			alive = false;
+		if !is_on_floor():	
+			air_time += 1
+			motion.x = 0
+			#para separar el cuerpor de las paredes al caer
+			if once:
+				self.position.x -= 13
+				once = false
+			$Sprite.scale.x = right
+			$AnimationPlayer.play("Fall")
+		else:
+			#reset la separación de la pared al tocar el suelo
+			once = true
+			air_time = 0
+			$Sprite.scale.x = right
+			$AnimationPlayer.play("Walking")
+			motion.x = MAXSPEED * right
+			#comprueba las colisiones durante el movimiento,
+			for i in get_slide_count():
+				var collision = get_slide_collision(i)
+				if collision: 
+					#si la colisión es diferente a la colisión frente al suelo, y frente a la dirección en la que avanzo
+					if collision.normal != Vector2(0,-1) and collision.normal == Vector2(-right,0):
+						right = -right
+	else:
+		air_time += 1;
+		if is_on_floor() or air_time > 600:
+			motion = Vector2();
+			$AnimationPlayer.play("Death")
+	move_and_slide(motion, Vector2.UP)
+
+#a esta función se llama desde la animación Death
+func death(): 
+	self.queue_free()
