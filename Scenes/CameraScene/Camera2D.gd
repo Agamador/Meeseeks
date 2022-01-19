f6c896f4abc7e973bbb1a4945b4a5079de68ebf2
Alejandro García <ale.garciamador@alum.uca.es>
2022-01-19 13:45:58 +0100

    Arreglo cámara al cambiar tamaño ventana

--- Scenes/CameraScene/Camera2D.gd
+++ Scenes/CameraScene/Camera2D.gd
@@ -22,8 +22,8 @@ func _ready():
 	#print(TileMapBorder.position);
 	self.limit_top = TileMapBorder.position.x*64;
 	self.limit_left = TileMapBorder.position.y *64;
-	self.limit_right = TileMapBorder.end.x*64 +64;
-	self.limit_bottom = TileMapBorder.end.y *64 +64;
+	self.limit_right = TileMapBorder.end.x*64;
+	self.limit_bottom = TileMapBorder.end.y *64;
 	#print(TileMapBorder.position.x);
 	viewport_size = get_viewport().size;
 	width_half = viewport_size.x/2;
@@ -34,17 +34,24 @@ func _ready():
 func _process(delta):
 	var move_vector = Vector2();
 	var mouse_pos = get_viewport().get_mouse_position();
-
-	if mouse_pos.x < 60 && self.position.x - w_h_times_zoom > self.limit_left:
-		move_vector.x = -1;
-	elif mouse_pos.x > viewport_size.x - 30 && self.position.x + w_h_times_zoom < self.limit_right:
+	_update_wiewport()
+	if mouse_pos.x < 30 && self.position.x - w_h_times_zoom > self.limit_left:
+			move_vector.x = -1;
+	elif mouse_pos.x > viewport_size.x - 15 && self.position.x + w_h_times_zoom < self.limit_right:
 		move_vector.x = 1;
-	if mouse_pos.y < 60 && self.position.y - h_h_times_zoom > self.limit_top:
+	if mouse_pos.y < 30 && self.position.y - h_h_times_zoom > self.limit_top:
 		move_vector.y = -1;
-	if mouse_pos.y > viewport_size.y - 30  && self.position.y + h_h_times_zoom < self.limit_bottom:
+	if mouse_pos.y > viewport_size.y - 15  && self.position.y + h_h_times_zoom < self.limit_bottom:
 		move_vector.y = 1;
 
 	global_translate(move_vector * delta * 300 * self.zoom.x)
+
+func _update_wiewport():
+	viewport_size = get_viewport().size;
+	width_half = viewport_size.x/2;
+	height_half = viewport_size.y/2;
+	w_h_times_zoom = width_half * self.zoom.x;
+	h_h_times_zoom = height_half * self.zoom.y;
 
 func _unhandled_input(event):
 	if event.is_action_pressed("zoom_in"):
@@ -64,9 +71,31 @@ func _set_zoom_level(value: float):
 		Tween.TRANS_SINE,
 		Tween.EASE_OUT);
 	tween.start();
-	
 
 
-func _on_DiggerButton_pressed():
-	print('botonsito')
-	get_parent().mouse_pointer = 'Digger';
+func _on_DiggerSideButton_pressed():
+	get_parent().mouse_pointer = 'DigSide';
+
+func _on_DiggerDownButton_pressed():
+	get_parent().mouse_pointer = 'DigDown';
+
+func _on_StopperButton_pressed():
+	get_parent().mouse_pointer = 'Stopper';
+
+func _on_UmbrellaButton_pressed():
+	get_parent().mouse_pointer = 'Umbrella';
+
+func _on_StairButton_pressed():
+	get_parent().mouse_pointer = 'Stair';
+
+func _on_ClimbButton_pressed():
+	get_parent().mouse_pointer = 'Climb';
+
+func _on_RestartButton_pressed():
+	pass
+
+func _on_TimeDownButton_pressed():
+	pass
+
+func _on_TimeUpButton_pressed():
+	pass
--- Scenes/Meeseek/Meeseek.gd
+++ Scenes/Meeseek/Meeseek.gd
@@ -25,7 +25,12 @@ func _ready():
 func _physics_process(delta):
 	match state:
 		'Basic': normal_meeseek(); 
-		'Digger': digger_meeseek()
+		'DigSide': digSide_meeseek()
+		'DigDown': pass
+		'Stopper': pass
+		'Umbrella': pass
+		'Stair': pass
+		'Climb': pass
 		_: normal_meeseek()
 	move_and_slide(motion, Vector2.UP)
 
@@ -73,8 +78,7 @@ func normal_meeseek():
 			$AnimationPlayer.play("Death")
 
 #selfxplaining
-#si eres excavador se aplica a todos los meeseeks?
-func digger_meeseek():
+func digSide_meeseek():
 	if(alive):
 		#bloque meeseek en el aire
 		#si vuela durante más de 2 segundos aprox(5 bloques)
@@ -125,20 +129,94 @@ func digger_meeseek():
 						else:
 							frames_digging = 0 
 							#5 a 7 son tiles destrozandose, 8 es el ultimo paso y -1 elimina la casilla
-							#posibilidad de cambiar por 9 a 11 (revisar numeros)
 							if first_collision >= 5 and first_collision <= 7:
 								first_collision = first_collision + 1
 								map.set_cellv(celda, first_collision+1)
 							elif first_collision == 8:
 								first_collision = -1
+								#añadir animación de romper bloque
 								map.set_cellv(celda, -1)
+								
 	else:
 		air_time += 1;
 		if is_on_floor() or air_time > 600:
 			motion = Vector2();
 			$AnimationPlayer.play("Death")
 			
+func digDown_meeseek():
+	if(alive):
+		#bloque meeseek en el aire
+		#si vuela durante más de 2 segundos aprox(5 bloques)
+		if air_time > 120: 
+			alive = false;
+		if !is_on_floor():	
+			air_time += 1
+			motion.x = 0
+			#para separar el cuerpor de las paredes al caer
+			if once:
+				if right > 0:
+					self.position.x += 13
+				else: 
+					self.position.x -= 13
+				once = false
+			$Sprite.scale.x = right
+			$AnimationPlayer.play("Fall")
+		#bloque meeseek en el suelo
+		else:
+			#reset la separación de la pared al tocar el suelo
+			once = true
+			air_time = 0
+			$Sprite.scale.x = right
+			$AnimationPlayer.play("Walking") 
+			motion.x = MAXSPEED * right
+			#comprueba las colisiones durante el movimiento,
+			for i in get_slide_count():
+				var collision = get_slide_collision(i)
+				if collision:
+					if collision.get_collider().get_class() == "StaticBody2D":
+						self.get_parent().meeseek_saved();
+						self.queue_free(); #borrado
+						#sumar meeseek a contador de exito y borrarlo
+					#si la colisión es diferente a la colisión frente al suelo, y frente a la dirección en la que avanzo
+					if collision.normal != Vector2(0,-1) and collision.normal == Vector2(-right,0):
+						celda = map.world_to_map(collision.position - collision.normal)
+						#print(map.get_cellv(celda))
+						#if first_collision != -1 and map.get_cellv(celda)
+						motion.x = 0
+						##inicia animación excavar
+						
+						
+						#numero de frames que se excavan 60 = 1 segundo
+						if frames_digging < 30:
+							frames_digging += 1
+							if first_collision == -1:
+								first_collision = map.get_cellv(celda)
+						else:
+							frames_digging = 0 
+							#5 a 7 son tiles destrozandose, 8 es el ultimo paso y -1 elimina la casilla
+							if first_collision >= 5 and first_collision <= 7:
+								first_collision = first_collision + 1
+								map.set_cellv(celda, first_collision+1)
+							elif first_collision == 8:
+								first_collision = -1
+								#añadir animación de romper bloque
+								map.set_cellv(celda, -1)
+								
+	else:
+		air_time += 1;
+		if is_on_floor() or air_time > 600:
+			motion = Vector2();
+			$AnimationPlayer.play("Death")
 			
+func stopper_meeseek():
+	pass
+func umbrella_meeseek():
+	pass
+func stair_meeseek():
+	pass
+func climb_meeseek():
+	pass
+	
 #a esta función se llama desde la animación Death para que el meeseek muera al terminar la animación
 func death(): 
 	get_parent().meeseek_deceased();
